1569d3806e55c1a95ac3f9d3dbd2917127ac02d0
Adedamola Shittu <shittuadedamola@rocketmail.com>
2022-01-10 18:24:41 +0100

    Create Actions For The Greeting Component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,16 +1,22 @@
 import React from "react"
-import PropTypes from "prop-types"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import PropTypes from "prop-types"
+import configureStore from '../redux/configureStore'
 import Greeting from './Greeting'
+
+const store = configureStore();
 
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={ <Greeting greeting="Friend"/> }></Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={ <Greeting /> }></Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
